53d874c47bcd519cb8c31c800b5c17f363d8fe03
Rakesh <tamangrakesh450@gmail.com>
2022-08-25 09:18:40 +0545

    themes extracted

---
+++ lib/widgets/themes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.amber,
+      appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+          textTheme: Theme.of(context).textTheme));
+
+  static ThemeData DarkTheme(BuildContext context) =>
+      ThemeData(brightness: Brightness.dark);
+}
